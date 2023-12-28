18df112efde420636d17f443806b9f759a777d99
jhkim31 <biz.jh.kim@gmail.com>
2023-12-28 23:17:45 +0900

    FEAT: jest test code * jest module test

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,20 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  roots: ["./test"],
+  roots: ["./test", "./src"],
   preset: 'ts-jest',
   testEnvironment: 'node',
+  reporters: [
+    "default",
+    ["jest-html-reporters", {
+      "publicPath": "./html-report",
+      "filename": "report.html",
+      "expand": true
+    }]
+  ],  
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*',
+    '!src/**/index.ts',
+    '!src/**/interfaces.ts'
+  ],
 };
--- test/a.test.ts
+++
@@ -1,9 +0,0 @@
-import request from "../src/core/request";
-
-
-test('a', () => {
-  request({
-    url: "https://www.naver.com",
-    method : "get"
-  })
-})
